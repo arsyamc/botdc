d4e2b50fa31fdb12e8147e62d08255f15b5c4e8f
ArsyaMC <121472019+arsyamc@users.noreply.github.com>
2024-06-22 17:02:07 +0700

    Update heading.tsx
    Change khodam to oshi

--- components/heading.tsx
+++ components/heading.tsx
@@ -4,7 +4,7 @@ import { subtitle, title } from "./primitives";
 export default function Heading() {
   return (
     <div className="inline-block max-w-lg text-center justify-center">
-      <h1 className={title()}>Check Khodam&nbsp;</h1>
+      <h1 className={title()}>Check Oshi&nbsp;</h1>
       <h1 className={title({ color: "violet" })}>Online</h1>
       <p className={subtitle({ class: "mt-2 text-base" })}>
         Cek Oshi yang ada pada tubuh seseorang melalui nama
