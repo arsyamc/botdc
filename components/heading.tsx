12463575af2adca79ecccc1f95669fcb80daa349
ArsyaMC <121472019+arsyamc@users.noreply.github.com>
2024-06-26 08:49:35 +0700

    Update heading.tsx

--- components/heading.tsx
+++ components/heading.tsx
@@ -5,7 +5,7 @@ export default function Heading() {
   return (
     <div className="inline-block max-w-lg text-center justify-center">
       <h1 className={title()}>Check Oshi&nbsp;</h1>
-      <h1 className={title({ color: "violet" })}>Online</h1>
+      <h1 className={title({ color: "gold" })}>Online</h1>
       <p className={subtitle({ class: "mt-2 text-base" })}>
         Cek Oshi seseorang melalui nama
       </p>
