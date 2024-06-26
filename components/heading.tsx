27ea10f9cdb220378d69d769bba3072774033b1c
ArsyaMC <121472019+arsyamc@users.noreply.github.com>
2024-06-26 08:54:32 +0700

    Update heading.tsx

--- components/heading.tsx
+++ components/heading.tsx
@@ -1,11 +1,11 @@
 import React from "react";
-import { subtitle, title } from "./primitives";
+import { subtitle, title, rainbowText } from "./primitives";
 
 export default function Heading() {
   return (
     <div className="inline-block max-w-lg text-center justify-center">
-      <h1 className={title()}>Check Oshi&nbsp;</h1>
-      <h1 className={title({ color: "violet" })}>Online</h1>
+      <h1 className={`${title()} ${rainbowText}`}>Check Oshi&nbsp;</h1>
+      <h1 className={`${title({ color: "violet" })} ${rainbowText}`}>Online</h1>
       <p className={subtitle({ class: "mt-2 text-base" })}>
         Cek Oshi seseorang melalui nama
       </p>
