ad72a2ef49010f5c6bd790d6cce699332a053cbf
ArsyaMC <121472019+arsyamc@users.noreply.github.com>
2024-08-05 13:08:37 +0700

    Update site.ts

--- config/site.ts
+++ config/site.ts
@@ -4,7 +4,7 @@ export const siteConfig = {
   links: {
     github: "https://github.com",
     tiktok: "https://tiktok.com/@_arsya_",
-    instagram: "https://instagram.com/_arsyaaa._",
+    instagram: "https://instagram.com/4arsya",
     discord: "https://discord.com/invite/7qkdQeajVb",
   },
 };
